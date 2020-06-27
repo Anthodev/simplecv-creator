7c4492fcae01ed9e73d1b227ab579d8146930d4a
Cedric Anthony <dev+git@anthodev.net>
2020-06-27 16:01:01 +0200

    Added Admin view + admin components

---
+++ assets/vue/components/admin/Information.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-tab-item value="tab-information">
+    <v-card flat>
+      <v-card-text>
+        <v-text-field placeholder="Name" filled></v-text-field>
+        <v-text-field placeholder="Title" filled></v-text-field>
+        <v-btn color="primary">Submit</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-tab-item>
+</template>
