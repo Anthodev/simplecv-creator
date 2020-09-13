9238d188c972b45727d878a96e73bbe1467c751a
Cedric Anthony <dev+git@anthodev.net>
2020-09-13 13:55:43 +0200

    Tweaked style for languages in homepage

--- assets/vue/components/Language.vue
+++ assets/vue/components/Language.vue
@@ -4,9 +4,8 @@
       <div class="text-h4 font-weight-bold text-uppercase section-title mb-2">Langues</div>
 
       <v-row v-for="(language) in languages" :key="language.id">
-        <v-col cols="12">
-          <div class="text-h6 font-weight-bold text-uppercase">{{ language.name }}</div>
-          <div class="text-overline text-text-uppercase mb-6">Niveau : <v-btn color="amber" small outlined>{{ language.level }}</v-btn></div>
+        <v-col cols="11" class="mx-auto">
+          <v-row class="text-h6 font-weight-bold text-uppercase d-flex align-center mb-6" justify="space-between"><span class="my-auto">{{ language.name }}</span><v-btn color="amber" small outlined>{{ language.level }}</v-btn></v-row>
           <v-divider/>
         </v-col>
       </v-row>
