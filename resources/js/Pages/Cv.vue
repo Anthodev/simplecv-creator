0170598a268243217320f6730d7e270abb23a4f5
Cedric Anthony <git@antho.dev>
2022-09-23 23:34:37 +0200

    🛠️ WIP: started front profil section

--- resources/js/Pages/Cv.vue
+++ resources/js/Pages/Cv.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import 'tw-elements';
+import {Head, Link, usePage} from '@inertiajs/inertia-vue3';
+
+let user = usePage().props.value.auth.user;
 
 let props = defineProps({
     canLogin: Boolean,
@@ -25,6 +28,14 @@ let props = defineProps({
 
                 <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
             </template>
+        </div>
+
+        <div class="container mx-auto dark:bg-gray-900 drop-shadow-lg">
+            <h1 class="font-mono font-medium leading-tight text-5xl mt-0 mb-2 border-l-8 pl-4 pb-1 dark:bg-gray-900 dark:text-white">PROFIL</h1>
+
+            <div class="mt-4 p-4 font-sans dark:text-white">
+                {{ user.description }}
+            </div>
         </div>
     </div>
 </template>
