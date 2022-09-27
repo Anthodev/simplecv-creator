3bc94ca58f0105517c40393d9fcae02ed7c97993
Cédric Anthony <Anthodev@users.noreply.github.com>
2022-09-27 19:39:33 +0200

    📦 NEW: Added support for link for skill and contact chips (#62)

--- resources/js/Components/Cv/SkillSection.vue
+++ resources/js/Components/Cv/SkillSection.vue
@@ -10,10 +10,16 @@ let props = defineProps({
 <template>
     <div v-if="Object.keys(props.skills).length > 0">
         <h5 v-if="title" class="block font-sans font-medium leading-tight text-xl mt-0 mb-2 border-r-8 border-b pl-4 pb-1 dark:text-white">{{ title }}</h5>
-        <div class="flex flex-wrap mt-3 gap-2 px-4">
-            <div v-for="(skill, index) in skills" :key="index" class="rounded-xl text-gray-500 bg-gray-200 font-semibold text-sm flex align-center cursor-pointer active:bg-gray-300 transition duration-300 ease w-max">
-                <span class="inline-block rounded-full max-w-none text-center p-1.5"><font-awesome-icon :icon="skill.icon" class="h-full align-middle" /></span>
-                <span class="flex items-center px-3 py-2 ml-4">{{ skill.name }}</span>
+        <div class="flex flex-wrap mt-3 gap-2 px-4 justify-center">
+            <div v-for="(skill, index) in skills" :key="index">
+                <a v-if="skill.url" :href="skill.url" target="_blank" class="ring ring-inset ring-amber-400 rounded-xl text-gray-500 dark:text-gray-200 bg-gray-200 dark:bg-slate-500 font-semibold text-sm flex align-center cursor-pointer active:bg-gray-300 transition duration-300 ease w-max">
+                    <span class="inline-block rounded-full max-w-none text-center p-1.5"><font-awesome-icon :icon="skill.icon" class="h-full align-middle" /></span>
+                    <span class="flex items-center px-3 py-2 ml-3">{{ skill.name }}</span>
+                </a>
+                <a v-else href="#" class="rounded-xl text-gray-500 bg-gray-200 font-semibold text-sm flex align-center cursor-pointer active:bg-gray-300 transition duration-300 ease w-max">
+                    <span class="inline-block rounded-full max-w-none text-center p-1.5"><font-awesome-icon :icon="skill.icon" class="h-full align-middle" /></span>
+                    <span class="flex items-center px-3 py-2 ml-3">{{ skill.name }}</span>
+                </a>
             </div>
         </div>
     </div>
