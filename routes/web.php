0f2704f8ae7d86e9aa8869035d587412ead89c3f
Cedric Anthony <git@antho.dev>
2022-09-29 12:15:03 +0200

    🛠️ WIP: updated routes

--- routes/web.php
+++ routes/web.php
@@ -22,35 +22,35 @@ use Inertia\Inertia;
 
 Route::get('/', [Controller::class, 'home'])->name('home');
 
-Route::post('users/{id}/update', [UserController::class, 'update'])->name('users.update');
+Route::post('users/{id}/update', [UserController::class, 'update'])->middleware(['auth', 'api'])->name('users.update');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::controller(ExperienceController::class)->middleware(['auth', 'verified'])->group(function () {
-    Route::get('experiences', 'index')->name('experiences.index');
+Route::get('experiences', [ExperienceController::class, 'index'])->middleware(['auth', 'verified'])->name('users.index');
+Route::controller(ExperienceController::class)->middleware(['auth', 'api'])->group(function () {
     Route::post('experiences/add', 'store')->name('experiences.store');
     Route::patch('experiences/{id}/update', 'update')->name('experiences.update');
     Route::delete('experiences/{id}/delete', 'destroy')->name('experiences.delete');
 });
 
-Route::controller(ContactController::class)->middleware(['auth', 'verified'])->group(function () {
-    Route::get('contacts', 'index')->name('contacts.index');
+Route::get('contacts', [ContactController::class, 'index'])->middleware(['auth', 'verified'])->name('contacts.index');
+Route::controller(ContactController::class)->middleware(['auth', 'api'])->group(function () {
     Route::post('contacts/add', 'store')->name('contacts.store');
     Route::patch('contacts/{id}/update', 'update')->name('contacts.update');
     Route::delete('contacts/{id}/delete', 'destroy')->name('contacts.delete');
 });
 
-Route::controller(ProjectController::class)->middleware(['auth', 'verified'])->group(function () {
-    Route::get('projects', 'index')->name('projects.index');
+Route::get('projects', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('projects.index');
+Route::controller(ProjectController::class)->middleware(['auth', 'api'])->group(function () {
     Route::post('projects/add', 'store')->name('projects.store');
     Route::post('projects/{id}/update', 'update')->name('projects.update');
     Route::delete('projects/{id}/delete', 'destroy')->name('projects.delete');
 });
 
-Route::controller(SkillController::class)->middleware(['auth', 'verified'])->group(function () {
-    Route::get('skills', 'index')->name('skills.index');
+Route::get('skills', [SkillController::class, 'index'])->middleware(['auth', 'verified'])->name('skills.index');
+Route::controller(SkillController::class)->middleware(['auth', 'api'])->group(function () {
     Route::post('skills/add', 'store')->name('skills.store');
     Route::patch('skills/{id}/update', 'update')->name('skills.update');
     Route::delete('skills/{id}/delete', 'destroy')->name('skills.delete');
